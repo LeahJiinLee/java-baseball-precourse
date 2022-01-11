ad3210f06eb989f36c6fa76eacf1449be23fb390
Ji In Lee <loveted0225@naver.com>
2022-01-11 10:30:10 +0900

    feat(Computer) : 컴퓨터 수 생성 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.ArrayList;
+import nextstep.utils.Randoms;
+
+public class Computer {
+    public ArrayList<Integer> computerNumberGenerator(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (numbers.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
+            }
+        }
+    return numbers;
+    }
+}
