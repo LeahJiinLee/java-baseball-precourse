f27e24bd49912192b1631bba8941739c53bade50
Ji In Lee <loveted0225@naver.com>
2022-01-11 14:22:19 +0900

    feat(Game): 숫장 야구 게임 로직 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import java.util.ArrayList;
+
+public class Game {
+    public void play(){
+        //게임 시작시
+
+        ArrayList<Integer> computerNumber;
+        ArrayList<Integer> userNumber;
+        int strikeCount;
+        int startOrEndFlag;
+        Computer computer= new Computer();
+        Player player = new Player();
+        Check check = new Check();
+        computerNumber= computer.computerNumberGenerator();  //컴퓨터 수 생성 -> 처음 생성되고 끝날 때까지 바뀌지 않음
+
+        while (true){
+            startOrEndFlag=0;
+            userNumber=player.playerNumber(); //사용자수 입력받기
+            strikeCount=check.checkNumber(computerNumber,userNumber);
+            if(strikeCount==3){
+                startOrEndFlag=player.startOrEnd();
+            }
+            if (startOrEndFlag==1) {
+                computerNumber= computer.computerNumberGenerator();
+                continue;
+            }
+            if (startOrEndFlag==2) {
+                System.out.println("게임 끝");
+                break;
+            }
+        }
+
+    }
+}
