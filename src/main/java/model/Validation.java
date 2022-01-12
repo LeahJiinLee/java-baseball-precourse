5facfd6d4691cd7d1e245065f0a3d10e5108b0cf
Ji In Lee <loveted0225@naver.com>
2022-01-12 16:12:48 +0900

    feat(Validation): 예외처리 5번(시작,종료 입력값 유효성체크) 기능 구현

--- src/main/java/model/Validation.java
+++ src/main/java/model/Validation.java
@@ -2,37 +2,49 @@ package model;
 
 import util.Message;
 import java.util.ArrayList;
+
 public class Validation {
-    public boolean checkInputLength(String userInput){
-        if (userInput.length()<3){
+
+    public boolean checkInputLength(String userInput) {
+        if (userInput.length() < 3) {
             Message.printLengthErrorMessage();
             return false;
         }
         return true;
     }
-    public boolean checkInputContainsZero(String userInput){
-        for (int i=0; i<userInput.length();i++){
-            if(userInput.charAt(i)=='0'){
+
+    public boolean checkInputContainsZero(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == '0') {
                 Message.printInputContainsZeroMessage();
                 return false;
             }
         }
         return true;
     }
-    public boolean checkInputFormat(String userInput){
-        for (int i=0;i<userInput.length(); i++){
-            if(!(userInput.charAt(i)>='0' && userInput.charAt(i)<='9')){
+
+    public boolean checkInputFormat(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (!(userInput.charAt(i) >= '0' && userInput.charAt(i) <= '9')) {
                 Message.printInputValidErrorMessage();
                 return false;
             }
         }
         return true;
     }
-    public boolean checkInputDuplicate(int userInputDigit, ArrayList<Integer> numbers){
-        if (numbers.contains(userInputDigit)){
+
+    public boolean checkInputDuplicate(int userInputDigit, ArrayList<Integer> numbers) {
+        if (numbers.contains(userInputDigit)) {
             Message.printDuplicateErrorMessage();
             return false;
         }
         return true;
+    }
+
+    public boolean checkStartOrEndFlag(int startOrEndFlag) {
+        if (startOrEndFlag == 1 || startOrEndFlag == 2) {
+            return true;
+        }
+        return false;
     }
 }
