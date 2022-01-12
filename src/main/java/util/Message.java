b61c6eef0c0d22b53bf1524209f7195f3fc7ae6b
Ji In Lee <loveted0225@naver.com>
2022-01-12 14:33:20 +0900

    feat(Message): 게임 종료, 시작시 메세지 추가

--- src/main/java/util/Message.java
+++ src/main/java/util/Message.java
@@ -10,5 +10,13 @@ public class Message {
     public static void printDuplicateErrorMessage(){
         System.out.println("ERROR: 각 자리가 중복되지 않게 입력해주세요!");
     }
-
+    public static void printGameStartMessage(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+    public static void printGameEndMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    public static void printGameExitMessage(){
+        System.out.println("게임 끝");
+    }
 }
