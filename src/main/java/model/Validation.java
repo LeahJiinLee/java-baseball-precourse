e1c7d246a5db221e48c7584b7c1d2f07f67e21b8
Ji In Lee <loveted0225@naver.com>
2022-01-12 15:04:19 +0900

    feat(validation): 0 입력시 예외 처리

--- src/main/java/model/Validation.java
+++ src/main/java/model/Validation.java
@@ -1,12 +1,21 @@
 package model;
 
-import view.Message;
+import util.Message;
 import java.util.ArrayList;
 public class Validation {
     public boolean checkInputLength(String userInput){
         if (userInput.length()<3){
             Message.printLengthErrorMessage();
             return false;
+        }
+        return true;
+    }
+    public boolean checkInputContainsZero(String userInput){
+        for (int i=0; i<userInput.length();i++){
+            if(userInput.charAt(i)=='0'){
+                Message.printInputContainsZeroMessage();
+                return false;
+            }
         }
         return true;
     }
