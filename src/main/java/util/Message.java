bfc656cf239ee0b708099cf18a001d6264e2beec
Ji In Lee <loveted0225@naver.com>
2022-01-12 16:13:45 +0900

    feat(Message): ERROR 메시지 추가

--- src/main/java/util/Message.java
+++ src/main/java/util/Message.java
@@ -1,22 +1,36 @@
 package util;
 
 public class Message {
-    public static void printInputMessage(){
+
+    public static void printInputMessage() {
         System.out.print("숫자를 입력해주세요 : ");
     }
-    public static void printLengthErrorMessage(){
+
+    public static void printLengthErrorMessage() {
         System.out.println("ERROR: 숫자 세개를 입력해주세요!");
     }
-    public static void printDuplicateErrorMessage(){
+
+    public static void printDuplicateErrorMessage() {
         System.out.println("ERROR: 각 자리가 중복되지 않게 입력해주세요!");
     }
-    public static void printGameStartMessage(){
+
+    public static void printInputContainsZeroMessage() {
+        System.out.println("ERROR: 각 자리의 수는 1~9사이의 숫자만 입력해주세요");
+    }
+
+    public static void printInputValidErrorMessage() {
+        System.out.println("ERROR: 숫자만 입력해주세요!");
+    }
+
+    public static void printGameStartMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-    public static void printGameEndMessage(){
+
+    public static void printGameEndMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
-    public static void printGameExitMessage(){
+
+    public static void printGameExitMessage() {
         System.out.println("게임 끝");
     }
 }
