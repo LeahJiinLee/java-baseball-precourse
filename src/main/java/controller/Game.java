a2b5d0e524bdfebf23012546552ad1293ea3a177
Ji In Lee <loveted0225@naver.com>
2022-01-12 16:14:35 +0900

    refactor(Game): 스트라이크, 볼 판단 메소드 분리에 따른 리팩토링

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -4,34 +4,48 @@ import model.Check;
 import model.Computer;
 import model.Player;
 import java.util.ArrayList;
+import util.Message;
+import view.OutputView;
 
 public class Game {
-    public void play(){
+
+    public void play() {
         //게임 시작시
 
         ArrayList<Integer> computerNumber;
         ArrayList<Integer> userNumber;
         int strikeCount;
+        int ballCount;
         int startOrEndFlag;
-        Computer computer= new Computer();
+        Computer computer = new Computer();
         Player player = new Player();
         Check check = new Check();
-        computerNumber= computer.computerNumberGenerator();  //컴퓨터 수 생성 -> 처음 생성되고 끝날 때까지 바뀌지 않음
+        OutputView outputView = new OutputView();
+        computerNumber = computer.computerNumberGenerator();  //컴퓨터 수 생성 -> 처음 생성되고 끝날 때까지 바뀌지 않음
 
-        while (true){
-            startOrEndFlag=0;
-            userNumber=player.playerNumber(); //사용자수 입력받기
-            if (userNumber.size()<3) continue;
-            strikeCount=check.checkNumber(computerNumber,userNumber);
-            if(strikeCount==3){
-                startOrEndFlag=player.startOrEnd();
-            }
-            if (startOrEndFlag==1) {
-                computerNumber= computer.computerNumberGenerator();
+        while (true) {
+            startOrEndFlag = 0;
+            Message.printInputMessage();
+            userNumber = player.playerNumber(); //사용자수 입력받기
+            if (userNumber.size() < 3) {
                 continue;
             }
-            if (startOrEndFlag==2) {
-                System.out.println("게임 끝");
+            strikeCount = check.checkStrikeCount(computerNumber, userNumber); //스트라이크 판단
+            ballCount = check.checkBallCount(computerNumber, userNumber); //볼 판단
+            if (strikeCount == 3) {
+                outputView.printThreeStrike();
+                Message.printGameEndMessage();
+                startOrEndFlag = player.startOrEnd();
+            }
+            if (strikeCount < 3) {
+                outputView.printResult(strikeCount, ballCount);
+            }
+            if (startOrEndFlag == 1) {
+                computerNumber = computer.computerNumberGenerator();
+                continue;
+            }
+            if (startOrEndFlag == 2) {
+                Message.printGameExitMessage();
                 break;
             }
         }
