95e934420244f3b43459e969e6a8ac23739613a2
Ji In Lee <loveted0225@naver.com>
2022-01-12 14:36:10 +0900

    refactor(Check): 스트라이크, 볼 판단 메서드 분리

--- src/main/java/model/Check.java
+++ src/main/java/model/Check.java
@@ -1,39 +1,26 @@
 package model;
 import java.util.ArrayList;
 public class Check {
-    public int checkNumber (ArrayList<Integer> computerNumber, ArrayList<Integer> userNumber){
-        //스트라이크 판단
-        //볼 판단
-        //낫싱 판단
+    //스트라이크 판단
+    //볼 판단
+    //낫싱 판단
+    public int checkStrikeCount (ArrayList<Integer> computerNumber, ArrayList<Integer> userNumber){
         int strikeCount=0;
-        int ballCount=0;
         for (int i=0; i<computerNumber.size(); i++){
             if (computerNumber.get(i) == userNumber.get(i)){
                 strikeCount++;
             }
         }
-        if (strikeCount==3){
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return strikeCount;
-        }
+        return strikeCount;
+    }
+    public int checkBallCount(ArrayList<Integer> computerNumber, ArrayList<Integer> userNumber){
+        int ballCount=0;
         for (int i=0; i<computerNumber.size(); i++){
             int checkIndex= userNumber.indexOf(computerNumber.get(i));
             if (checkIndex>-1 && checkIndex!=i){
                 ballCount++;
             }
         }
-        if(strikeCount>0){
-            System.out.print(strikeCount + "스트라이크 ");
-        }
-        if (ballCount>0 ){
-            System.out.print(ballCount + "볼");
-        }
-        if (ballCount ==0 && strikeCount==0) {
-            System.out.print("낫싱");
-        }
-        System.out.println();
-
-        return strikeCount;
+        return ballCount;
     }
 }
